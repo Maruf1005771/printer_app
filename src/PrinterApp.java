42b731667f93c8d2564179544578d9a4a6e57c29
Your Name <amk1005771@student.hamk.fi>
2024-10-20 19:45:55 +0300

    full assignment done

--- src/PrinterApp.java
+++ src/PrinterApp.java
@@ -7,6 +7,41 @@ public class PrinterApp {
 
         int number2 = 2;
 
+        int multiplication;
+
+        int addition;
+        
+        int subtraction;
+        
+        double division;
+
+
+
+        System.out.println("Printer App");
+
+       
+        System.out.print("This app is made by: ");
+        
+        System.out.println(creator);
+
+       
+        
+        System.out.println("The value of number1 is " + number1);
+
+        System.out.println("The value of number2 is " + number2);
+
+        multiplication = number1 * number2;
+
+        System.out.println(number1 + " * " + number2 + " = " + multiplication);
+
+        addition = number1 + number2;
+        System.out.println(number1 + " + " + number2 + " = " + addition);
+
+        subtraction = number1 - number2;
+        System.out.println(number1 + " - " + number2 + " = " + subtraction);
+
+        division = (double) number1 / number2;
+        System.out.println(number1 + " / " + number2 + " = " + division);
 
 
     }
