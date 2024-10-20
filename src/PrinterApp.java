fa272b7f1dba6aac98a565b9df7ee9c98e47b8be
Your Name <amk1005771@student.hamk.fi>
2024-10-20 19:27:47 +0300

    Initial commit

---
+++ src/PrinterApp.java
@@ -0,0 +1,13 @@
+public class PrinterApp {
+    public static void main(String[] args) {
+    
+        String creator = " Md Maruf";
+
+        int number1 = 5;
+
+        int number2 = 2;
+
+
+
+    }
+}
